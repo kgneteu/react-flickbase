6fce6633b805c3d02f11c50517370cde0b5da2e4
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-10-06 11:34:55 +0200

    Heroku updates

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ mongoose.connect(mongoURI, err => {
 })
 
 app.use(express.static('client/build'))
+
+if (process.env.NODE_ENV === 'production'){
+  const path = require('path')
+  app.get('/*',(req, res)=>{
+      res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+  })
+
+}
 const port = process.env.port || 3001
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`)
